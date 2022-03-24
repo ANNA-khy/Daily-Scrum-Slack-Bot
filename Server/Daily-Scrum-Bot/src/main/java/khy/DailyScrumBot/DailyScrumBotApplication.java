cb87756a69b3c1f3edc44c22d0a1f84c4dc8f2fe
ANNA-khy <kang01019@naver.com>
2022-03-24 22:10:28 +0900

    Spring file 추가

---
+++ Server/Daily-Scrum-Bot/src/main/java/khy/DailyScrumBot/DailyScrumBotApplication.java
@@ -0,0 +1,13 @@
+package khy.DailyScrumBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyScrumBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyScrumBotApplication.class, args);
+	}
+
+}
---
+++ Server/Daily-Scrum-Bot/src/test/java/khy/DailyScrumBot/DailyScrumBotApplicationTests.java
@@ -0,0 +1,13 @@
+package khy.DailyScrumBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyScrumBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
