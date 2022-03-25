5df438b29f1ce50e5df6d0eaa1148e9012da1dc8
ANNA-khy <kang01019@naver.com>
2022-03-25 21:15:05 +0900

    /Server로 spring 파일 위치 변경

---
+++ Server/src/test/java/khy/DailyScrumBot/DailyScrumBotApplicationTests.java
@@ -0,0 +1,14 @@
+package khy.DailyScrumBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyScrumBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert 3==3;
+	}
+
+}
